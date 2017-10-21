f197bbe63e5ebe3e315780f6572e3caef4c51dd7
Nélio Mavie <neliomelmavie@gmail.com>
2017-10-21 02:33:33 +0200

    Aplicacao Estável

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -1,34 +1,50 @@
 @extends('layouts.master')
 
 @section('title')
-    Eventos Do Museu
+    Eventos
 @stop
 
 @section('content')
-      <div class="default-tab-area clearfix">
-        <div class="container">
-          <div class="row">
-            
-            <h3>Eventos</h3>
 
+<!-- Blog Section -->
+    <section id="blog" class="section">
+      <!-- Container Starts -->
+      <div class="container">
+        <!-- Row Starts -->
+        <div class="row">      
+          @forelse($eventos as $evento)
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <!-- Blog Item Wrapper Starts-->
+            <div class="blog-item-wrapper wow fadeIn" data-wow-delay="0.9s">
+              <div class="blog-item-img">
+                <a href="#">
+                	<img src="/evento_upload/{{ $evento->imagem}}" alt="">
+                	@if(!$evento->imagem)
+                		Sem Imagem
+                	@endif
+                </a>
+              </div>
+              <div class="blog-item-text">
+                <h3 class="small-title"><a href="#">{{ $evento->nome}}</a></h3>
+                <p>
+                   {{ $evento->descricao}}
+                </p>
+                <div class="blog-one-footer">
+                  <a href="#"><i class="fa fa-eye"></i>Ver Evento</a>
+                  <a href="#"><i class="icon-heart"></i> 59 Likes</a>
+                  <a href="#"><i class="icon-bubbles"></i> 120 Comments</a>                  
+                </div>
+              </div>
+            </div><!-- Blog Item Wrapper Ends-->
           </div>
-        </div>
-      </div>
+          @empty
+          @endforelse
+        </div><!-- Row Ends -->
+      </div><!-- Container Ends -->
+    </section>
+    <!-- blog Section End -->
 
 @endsection
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
