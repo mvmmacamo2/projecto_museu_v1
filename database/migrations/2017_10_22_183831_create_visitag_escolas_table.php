ededd0429ebb9892ce499a7bbcbf1524f82b7722
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-22 11:40:29 -0700

    criando Model e Migration  Visitas guiadas

---
+++ database/migrations/2017_10_22_183831_create_visitag_escolas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitagEscolasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('visitag_escolas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('data');
+            $table->string('hora');
+            $table->string('descricao');
+            $table->integer('escola_id')->unsigned();
+            $table->foreign('escola_id')->references('id')->on('escolas')->onDelete('NO ACTION');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visitag_escolas');
+    }
+}
