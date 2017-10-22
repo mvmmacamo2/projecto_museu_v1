cdeb0c0686a69c1294587a4924fdd80680971ebe
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-22 14:03:36 -0700

    adicionando a lista de pedidos de escolas . este é um método

--- app/Http/Controllers/VisitaGEscolasController.php
+++ app/Http/Controllers/VisitaGEscolasController.php
@@ -7,7 +7,20 @@ use Illuminate\Http\Request;
 class VisitaGEscolasController extends Controller
 {
     public function visitaguiadaescolar()
+
     {
+
+        $pedidos= DB::table('view_visitasgescolas')->get();
+//
+//       // dd($pedidos);
+//        return view('visitasg.escola', compact('pedidos'));
+
+
+        $pedidos= Vvisita::all();
+
+        //dd($pedidos);
+      //  return view('visitasg.escola', compact('pedidos'));
+
         return view('admins.pedidos.visitasgescolas');
     }
 }
