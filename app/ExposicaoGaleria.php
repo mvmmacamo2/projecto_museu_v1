a594c2430312bc00d29cf55ac2217b11f67703eb
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-22 12:48:33 -0700

    Relacionando a Exposicao com sua Respectiva Galeria

--- app/ExposicaoGaleria.php
+++ app/ExposicaoGaleria.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Exposicao;
 
 class ExposicaoGaleria extends Model
 {
-    //
+    public function galeriaexposicoes()
+    {
+       return $this->hasMany(Exposicao::class);
+    }
 }
