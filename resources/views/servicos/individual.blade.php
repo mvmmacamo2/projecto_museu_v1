8f7c258ac6b5d2c16980b2f975e5fd14e6857dec
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-22 13:31:30 -0700

    Organizando as View dos Servocos

--- resources/views/servicos/individual.blade.php
+++ resources/views/servicos/individual.blade.php
@@ -1,28 +1,39 @@
 @extends('layouts.master')
 
+
+
+
 @section('title')
     Requisitando Visita Guiada Individual
 @stop
-
 @section('content')
 
-<!-- Blog Section -->
-    <section id="blog" class="section">
-      <!-- Container Starts -->
-      <div class="container">
-        <!-- Row Starts -->
-        <div class="row">      
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-            <!-- Blog Item Wrapper Starts-->
-            
-          </div>
-        </div><!-- Row Ends -->
-      </div><!-- Container Ends -->
-    </section>
-    <!-- blog Section End -->
+    <form method="post" action="">
 
-@endsection
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="data">Data Realizacao do Evento</label>
+                <input type="date" class="form-control" id="data" placeholder="" name="data">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="hora">Hora Da Visita</label>
+                <input type="text" class="form-control" id="hora" placeholder="exemplo: 10h" name="hora">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="inputAddress">Descricao da Visita</label>
+
+            <textarea class="form-control" cols="10" rows="5" placeholder="Coloque o motivo da Visita" name="descricao">
+
+            </textarea>
+
+        </div>
 
 
+        <button type="submit" class="btn btn-success">Submeter</button>
+    </form>
 
 
+@stop
+<br>
