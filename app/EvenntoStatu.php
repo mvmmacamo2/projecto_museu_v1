e3b574e10ce216d23ec23d95da1e80b300fbb034
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-22 12:37:49 -0700

    Retificando o Modelo status

--- app/EvenntoStatu.php
+++ app/EvenntoStatu.php
@@ -10,6 +10,7 @@ class EvenntoStatu extends Model
 {
     public function eventoestados()
     {
-      return $this->hasMany(User::class, Evento::class);
+        return $this->hasMany(User::class);
+        return $this->hasMany(Evento::class);
     }
 }
--- app/Publicar.php
+++ app/Publicar.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use  App\User;
+
 
 class Publicar extends Model
 {
-    //
+    public function publicacoes()
+    {
+        return $this->hasMany(User::class);
+    }
 }
