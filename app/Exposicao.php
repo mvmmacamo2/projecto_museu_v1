25feb5e94cc62b449532f777291570e68cc14c34
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-22 12:52:34 -0700

    Relacionando o Usuario a Exposicoes

--- app/Exposicao.php
+++ app/Exposicao.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Exposicao extends Model
 {
+
+    public function exposicoes()
+    {
+        return $this->hasMany(User::class);
+    }
+
     protected $fillable = [
         'nome', 'descricao', 'imagem', 'estado',
     ];
