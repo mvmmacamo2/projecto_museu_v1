658e740fdc4ead574775ee169a8e6ce7b668d82d
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-23 15:00:00 -0700

    adicionando os names nos campos de user pedido e campo ihidden com o seu id

--- resources/views/servicos/individual.blade.php
+++ resources/views/servicos/individual.blade.php
@@ -13,12 +13,14 @@
         <!-- Row Starts -->
         <div class="row">      
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
                 <form>
+
                   <div class="row">
 
                     <div class="col-md-3 mb-3">
                       <label for="descricao">Data Realizacao do Evento</label>
-                      <input type="date" class="form-control" id="descricao" required>
+                      <input type="date" class="form-control" id="descricao" required name="data">
                       <div class="invalid-feedback">
                         Introduza uma Data Válida..!
                       </div>
@@ -26,15 +28,15 @@
 
                     <div class="col-md-3 mb-3">
                         <label for="hora">Hora Da Visita</label>
-                        <input type="time" class="form-control" id="hora" required>
+                        <input type="time" class="form-control" id="hora" required name="hora">
                         <div class="invalid-feedback">
                             Introduza uma Hora Válida..!
                         </div>
                     </div>
-
+                    <input type="hidden" name="usuario_id" value="{{ Auth()->user()->id }}">
                     <div class="col-md-6 mb-3">
                         <label for="descricao">Descrição da Visita</label>
-                        <textarea style="min-height: 100px" class="form-control" id="descricao" required></textarea>
+                        <textarea style="min-height: 100px" class="form-control" id="descricao" required name="descricao"></textarea>
                         </div>
                     </div>
 
