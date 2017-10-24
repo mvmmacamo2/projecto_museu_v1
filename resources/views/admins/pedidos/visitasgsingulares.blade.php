9bff8b930059d436f0298f7a60e6fbeaae3b75bc
Nélio Mavie <neliomelmavie@gmail.com>
2017-10-24 11:38:40 +0200

    Pedidos - Form de resposta ao pedido

--- resources/views/admins/pedidos/visitasgsingulares.blade.php
+++ resources/views/admins/pedidos/visitasgsingulares.blade.php
@@ -83,7 +83,35 @@
 
                                 <td>
                                     <a href="#">
-                                    <button class="btn btn-success"> Responder</button>
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Responder</button>
+
+                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                          <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                              <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                  <span aria-hidden="true">&times;</span>
+                                                </button>
+                                              </div>
+                                              <div class="modal-body">
+                                                <form>
+                                                  <div class="form-group">
+                                                    <label for="recipient-name" class="col-form-label">E-mail:</label>
+                                                    <input type="text" class="form-control" id="recipient-name">
+                                                  </div>
+                                                  <div class="form-group">
+                                                    <label for="message-text" class="col-form-label">Messagem:</label>
+                                                    <textarea class="form-control" id="message-text"></textarea>
+                                                  </div>
+                                                </form>
+                                              </div>
+                                              <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Enviar</button>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
                                     </a>
                                     <button class="btn btn-danger"><i class="fa fa-trash"></i> Apagar</button>
                                 </td>
