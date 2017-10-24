1e2bba170947f3577d896e6d701cbd2800ca62a6
Nélio Mavie <neliomelmavie@gmail.com>
2017-10-24 11:13:41 +0200

    Ver pedido - Melhoramentos

--- resources/views/admins/pedidos/visitasgescolas.blade.php
+++ resources/views/admins/pedidos/visitasgescolas.blade.php
@@ -52,7 +52,35 @@
 
                                 <td>{{$p->hora}}</td>
                                 <td>{{$p->created_at}}</td>
-                                <td>{{$p->descricao}}</td>
+                                <td>
+                                    <a href="#">
+                                        <!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                                          Ver Descrição <i class="fa fa-eye"></i>
+                                        </button>
+
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                                          <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                              <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLongTitle">Descrição Do Pedido</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                  <span aria-hidden="true">&times;</span>
+                                                </button>
+                                              </div>
+                                              <div class="modal-body">
+                                                    {{$p->descricao}}.. 
+                                              </div>
+                                              <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </a>
+                                </td>
 
 
                                 {{--<td>--}}
