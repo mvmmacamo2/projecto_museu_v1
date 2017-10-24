c3829a024f8320576b68bad8025170382aa996eb
Nélio Mavie <neliomelmavie@gmail.com>
2017-10-24 16:01:01 +0200

    Registando middlewares

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,11 +19,6 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $exposicoes = Exposicao::all();
