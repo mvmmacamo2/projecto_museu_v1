1d5c13ea4b275b254b4f076f6e180ee25ea95579
Nélio Mavie <neliomelmavie@gmail.com>
2017-10-24 16:01:26 +0200

    Criando Middlewares

---
+++ app/Http/Middleware/Autenticacao.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class Autenticacao
+{
+
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check())
+            return $next($request);
+        return redirect('/login');
+    }
+}
