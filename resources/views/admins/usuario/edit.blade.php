c936fae705ac3957cdfb1cc8701e92e2c7814638
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-25 01:14:52 -0700

    lista de usuarios numa boa

--- resources/views/admins/usuario/edit.blade.php
+++ resources/views/admins/usuario/edit.blade.php
@@ -4,7 +4,7 @@
 
     <div class="row">
         <div class="col-lg-10">
-            <h1 class="page-header">Visita Guiada Escolar</h1>
+            <h1 class="page-header">Actualizar Informacoes do User</h1>
         </div>
 
     </div>
@@ -20,12 +20,68 @@
 
                 <!-- /.panel-heading -->
                 <div class="panel-body">
-                    
+                    <form>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="nome">Nome</label>
+                                <input type="text" class="form-control" id="nome" placeholder=""
+                                       value="{{$usuario->name}}" readonly name="name">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputPassword4">Username</label>
+                                <input type="text" class="form-control" id="username" placeholder="Password"
+                                       name="username" value="{{$usuario->username}}" readonly>
+
+                            </div>
+
+
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="nome">Nivel Acesso</label>
+                                    <input type="text" class="form-control" id="nome" placeholder=""
+                                           value="{{$usuario->nivelacesso}}" readonly name="nivel">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Estado</label>
+                                    <input type="text" class="form-control" id="username" placeholder="Password"
+                                           name="username" value="{{$usuario->estado}}" readonly>
+
+                                </div>
+
+                            </div>
+
+                            <div class="form-row">
+
+                                <div class="form-group col-md-6">
+                                    <label for="nome">Estado</label>
+                                    <select class="form-control">
+                                        <option value="Activa">Activar</option>
+                                        <option value="Desactivar">Desactivar</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="nome">Nível Acesso</label>
+                                    <select class="form-control">
+                                        <option value="Normal">Normal</option>
+                                        <option value="Administrador">Administrador</option>
+
+                                    </select>
+                                </div>
+
+
+                                <button type="submit" class="btn btn-primary">Actualizar</button>
+                            </div>
+                        </div>
+                    </form>
+
                 </div>
-                <!-- /.panel-body -->
+
             </div>
-            <!-- /.panel -->
+            <!-- /.panel-body -->
         </div>
-        <!-- /.col-lg-12 -->
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
     </div>
 @stop
