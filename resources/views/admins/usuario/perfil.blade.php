5ac3508d3f97af7395879955d64a342cbc081315
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-27 02:41:02 -0700

    alerando o formulario de perfil

--- resources/views/admins/usuario/perfil.blade.php
+++ resources/views/admins/usuario/perfil.blade.php
@@ -37,8 +37,46 @@ xzhcxzhcv
 
                 <!-- /.panel-heading -->
                 <div class="panel-body">
-                  sdgashdgfasgdvdbsvc
+                    <form>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="nome">Nome</label>
+                                <input type="text" class="form-control" id="nome" placeholder="Email" value="name">
+                            </div>
 
+                            <div class="form-group col-md-6">
+                                <label for="username">UserName</label>
+                                <input type="text" class="form-control" id="username" placeholder="Password">
+                            </div>
+                        </div>
+
+
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputCity">City</label>
+                                <input type="text" class="form-control" id="inputCity">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="inputState">State</label>
+                                <select id="inputState" class="form-control">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label for="inputid">ID</label>
+                                <input type="text" class="form-control" id="inputid">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-check">
+                                <label class="form-check-label">
+                                    <input class="form-check-input" type="checkbox"> Check me out
+                                </label>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Sign in</button>
+                    </form>
                 </div>
                 <!-- /.panel-body -->
             </div>
