35bd28bdb94be151fba57263abcc80174432e727
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-28 02:41:47 -0700

    Comentando os Schemas

--- database/migrations/2017_10_28_093143_create_vgalerias_table.php
+++ database/migrations/2017_10_28_093143_create_vgalerias_table.php
@@ -19,10 +19,10 @@ class CreateVgaleriasTable extends Migration
       SELECT  u.name, u.email , v.data ,v.hora, v.descricao, v.estado,v.created_at FROM apatios v, users u WHERE u.id=v.usuario_id
       )
         ");
-        Schema::create('vgalerias', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
+//        Schema::create('vgalerias', function (Blueprint $table) {
+//            $table->increments('id');
+//            $table->timestamps();
+//        });
     }
 
     /**
@@ -33,6 +33,6 @@ class CreateVgaleriasTable extends Migration
     public function down()
     {
         DB::statement('DROP VIEW IF EXISTS v__patios');
-        Schema::dropIfExists('vgalerias');
+//        Schema::dropIfExists('vgalerias');
     }
 }
