551593b9ac30f152f8f906b689a0e42edaf91648
Nélio Mavie <neliomelmavie@gmail.com>
2017-10-29 08:20:51 +0200

    index principal melhorando

--- resources/views/template_welcome.blade.php
+++ resources/views/template_welcome.blade.php
@@ -553,40 +553,6 @@
     </section>
     <!-- blog Section End -->
 
-    <!-- Clients Section -->
-    <section id="clients" class="section">
-      <!-- Container Ends -->
-      <div class="container">
-        <h1 class="section-title wow fadeInUpQuick" data-wow-delay=".5s">
-          CLIENTS
-        </h1>
-        <p class="section-subcontent">At vero eos et accusamus et iusto odio dignissimos ducimus qui <br> blanditiis praesentium</p>
-        <div class="wow fadeInUpQuick" data-wow-delay=".9s">
-          <!-- Row and Scroller Wrapper Starts -->
-          <div class="row" id="clients-scroller">
-            <div class="client-item-wrapper">
-              <img src="/engage/assets/img/clients/img1.png" alt="">
-            </div>
-            <div class="client-item-wrapper">
-              <img src="/engage/assets/img/clients/img2.png" alt="">
-            </div>
-            <div class="client-item-wrapper">
-              <img src="/engage/assets/img/clients/img3.png" alt="">
-            </div>
-            <div class="client-item-wrapper">
-              <img src="/engage/assets/img/clients/img4.png" alt="">
-            </div>
-            <div class="client-item-wrapper">
-              <img src="/engage/assets/img/clients/img5.png" alt="">
-            </div>
-            <div class="client-item-wrapper">
-              <img src="/engage/assets/img/clients/img6.png" alt="">
-            </div>
-          </div><!-- Row and Scroller Wrapper Starts -->
-        </div>
-      </div><!-- Container Ends -->
-    </section>
-    <!-- Client Section End -->        
     
     <!-- Footer Section -->
     <footer>
