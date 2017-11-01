43207f80e71c42cf63d5de23c3d3e132565c7308
Nélio Mavie <neliomelmavie@gmail.com>
2017-11-01 05:35:26 +0200

    Criando uma rota temporaria

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,4 +41,9 @@ class HomeController extends Controller
         //     return view('template_welcome');
         // }
     }
+
+    public function org(){
+
+        return view('auth.register_org');
+    }
 }
