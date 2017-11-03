c71a31afe4615976c19f1de68cc8bb638b97574d
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-03 12:07:39 -0700

    adicionando os Relacionamentos de exposicao

--- app/LikeExposicao.php
+++ app/LikeExposicao.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\ComtarioExposicao;
 
 class LikeExposicao extends Model
 {
+    public function likeexposicao()
+    {
+        return $this->hasMany(User::class);
+    }
 
+    public function comentarioexposicao()
+    {
+        return $this->hasMany(User::class);
+    }
 }
