96f0d643aaee8b71f1c40fa35f0dd78cd4279219
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-03 01:47:07 -0700

    criando o escolaController e as suas funçoes

---
+++ app/Http/Controllers/EscolaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EscolaController extends Controller
+{
+
+	public function index()
+	{
+		return 'existe dados de escola';
+	}
+
+	public function login()
+	{
+		return 'existe dados de escola';
+	}
+
+
+	public function postlogin()
+	{
+		return 'existe dados de escola';
+	}
+}
