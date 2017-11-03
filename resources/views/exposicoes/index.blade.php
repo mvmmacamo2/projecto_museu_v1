d40573479dd68e012db0906db8e63399a48f6ada
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-03 15:04:10 -0700

    Merge branch 'master' of https://github.com/neliomavie/projecto_museu_v1

--- resources/views/exposicoes/index.blade.php
+++ resources/views/exposicoes/index.blade.php
@@ -1,45 +1,45 @@
 @extends('layouts.master')
 
 @section('title')
-    Exposições
+Exposições
 @stop
 
 @section('content')
 
 <!-- Blog Section -->
-    <section id="blog" class="section">
-      <!-- Container Starts -->
-      <div class="container">
-        <!-- Row Starts -->
-        <div class="row">      
-          @forelse($exposicoes as $exposicao)
-          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-            <!-- Blog Item Wrapper Starts-->
-            <div class="blog-item-wrapper wow fadeIn" data-wow-delay="0.9s">
-              <div class="blog-item-img">
-                <a href="/usuario_galerias/{{ $exposicao->id }}">
-                  <img src="/expo-upload/{{ $exposicao->imagem}}" alt="">
-                </a>
-              </div>
-              <div class="blog-item-text">
-                <h3 class="small-title"><a href="#">{{ $exposicao->nome}}</a></h3>
-                <p>
-                   {{ $exposicao->descricao}}
-                </p>
-                <div class="blog-one-footer">
-                  <a href="/usuario_galerias/{{ $exposicao->id }}"><i class="fa fa-eye"></i>Ver Exposição</a>
-                  <a href="#"><i class="icon-heart"></i> 59 Likes</a>
-                  <a href="#"><i class="icon-bubbles"></i> 120 Comments</a>                  
-                </div>
-              </div>
-            </div><!-- Blog Item Wrapper Ends-->
+<section id="blog" class="section">
+  <!-- Container Starts -->
+  <div class="container">
+    <!-- Row Starts -->
+    <div class="row">      
+      @forelse($exposicoes as $exposicao)
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <!-- Blog Item Wrapper Starts-->
+        <div class="blog-item-wrapper wow fadeIn" data-wow-delay="0.9s">
+          <div class="blog-item-img">
+            <a href="/usuario_galerias/{{ $exposicao->id }}">
+              <img src="/expo-upload/{{ $exposicao->imagem}}" alt="">
+            </a>
           </div>
-          @empty
-          @endforelse
-        </div><!-- Row Ends -->
-      </div><!-- Container Ends -->
-    </section>
-    <!-- blog Section End -->
+          <div class="blog-item-text">
+            <h3 class="small-title"><a href="#">{{ $exposicao->nome}}</a></h3>
+            <p>
+             {{ $exposicao->descricao}}
+           </p>
+           <div class="blog-one-footer">
+            <a href="/usuario_galerias/{{ $exposicao->id }}"><i class="fa fa-eye"></i>Ver Exposição</a>
+            <a href="#"><i class="icon-heart"></i> 59 Likes</a>
+            <a href="#"><i class="icon-bubbles"></i> 120 Comments</a>                  
+          </div>
+        </div>
+      </div><!-- Blog Item Wrapper Ends-->
+    </div>
+    @empty
+    @endforelse
+  </div><!-- Row Ends -->
+</div><!-- Container Ends -->
+</section>
+<!-- blog Section End -->
 
 @endsection
 
