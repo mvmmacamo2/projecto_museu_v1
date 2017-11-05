354d36578744c877c4455f663c503437df7ff8a2
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-05 03:12:12 -0800

    Verificando o codigo de likes duplos

--- app/Http/Controllers/LikeExposicaoController.php
+++ app/Http/Controllers/LikeExposicaoController.php
@@ -16,24 +16,42 @@ class LikeExposicaoController extends Controller
 		$usuario =User::all();
 		$exposicao= Exposicao::all();
 		$likeexposicao= LikeExposicao::all();
+		
 
-		foreach ($usuario as $u) {
-			foreach ($exposicao as $ex) {
-				foreach ($likeexposicao as $like) {
-					if (($u->id=$like->usuario_id) and ($ex->id=$like->exposicao_id)) {
+		empty(auth()->user()->id){
 
+		}else{
+			$user = auth()->user()->id;
+			foreach ($likeexposicao as $like) {
+				foreach ($exposicao as $ex) {
+					if (($user=$like->usuario_id) AND ($ex->id=$like->exposicao_id) ) {
 						return redirect('/');
 					}else{
 						$salvar = LikeExposicao::create($request->all());
 					}
-
-
 				}
+				
 			}
-
+			
 		}
-		
-		
+
+		// foreach ($usuario as $u) {
+		// 	foreach ($exposicao as $ex) {
+		// 		foreach ($likeexposicao as $like) {
+		// 			if (($u->id=$like->usuario_id) and ($ex->id=$like->exposicao_id)) {
+
+		// 				return redirect('/');
+		// 			}else{
+		// 				$salvar = LikeExposicao::create($request->all());
+		// 			}
+
+
+		// 		}
+		// 	}
+
+		// }
+
+
 	}
 
 }
