7a766d03ffc5fa6bab67a18c024811b3597a78c3
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-05 02:59:57 -0800

    mandando dados para a tela inicial

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Exposicao;
 use App\Evento;
 use App\User;
+use DB;
 
 class HomeController extends Controller
 {
@@ -25,22 +26,25 @@ class HomeController extends Controller
         $eventos = Evento::all();
         $usuarios = User::all();
         $u = Auth()->user();
+        $likesexposicao =DB::table('like_exposicaos')->get();
+        $comentexposicao =DB::table('comtario_exposicaos')->get();
        // return $u->nivelacesso;
         $nivel = $u->nivelacesso;
 
         if ($nivel == 'Administrador') {
             return view('home-admin', compact('exposicoes', 'eventos','usuarios'));
         }elseif ($nivel=='Normal') {
-               return view('welcome', compact('exposicoes', 'eventos','usuarios')); 
-        }else{
-            return view('welcome', compact('exposicoes', 'eventos','usuarios'));
+         return view('welcome', compact('exposicoes', 'eventos','usuarios'))->with('nrlike',$likesexposicao)->with('comentexp', $comentexposicao);
+         ; 
+     }else{
+        return view('welcome', compact('exposicoes', 'eventos','usuarios'));
 
-        }
+    }
 
         // if ($nivel=='Normal'){
         //     return view('template_welcome');
         // }
-    }
+}
 
-    
+
 }
