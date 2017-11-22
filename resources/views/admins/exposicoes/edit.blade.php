d36f0fb99bb5a4a6c155d52207e06f2e6ca1853e
Nélio Mavie <neliomelmavie@gmail.com>
2017-11-22 23:17:21 +0200

    Trabalhando o template do admin6

--- resources/views/admins/exposicoes/edit.blade.php
+++ resources/views/admins/exposicoes/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('master-admin')
+@extends('layouts.master-admin')
 
 @section('content-admin')
 <div class="row">
@@ -16,62 +16,62 @@
             </div>
             <!-- /.panel-heading -->
             <div class="panel-body">
-            <form class="form-horizontal" method="POST" action="/exposicoes/{{$exposicao->id}}" enctype="multipart/form-data">  
-                {{ method_field('PATCH') }}
-                {{ csrf_field() }}
+                <form class="form-horizontal" method="POST" action="/exposicoes/{{$exposicao->id}}" enctype="multipart/form-data">  
+                    {{ method_field('PATCH') }}
+                    {{ csrf_field() }}
 
-            <input type="hidden" name="estado" value="{{ $exposicao->estado }}">
+                    <input type="hidden" name="estado" value="{{ $exposicao->estado }}">
 
-            <div class="form-group{{ $errors->has('nome') ? ' has-error' : '' }}">
-                <label for="nome" class="col-md-4 control-label">Nome</label>
+                    <div class="form-group{{ $errors->has('nome') ? ' has-error' : '' }}">
+                        <label for="nome" class="col-md-4 control-label">Nome</label>
 
-                <div class="col-md-6">
-                    <input id="nome" type="text" class="form-control" name="nome" value="{{ $exposicao->nome }}" required autofocus>
+                        <div class="col-md-6">
+                            <input id="nome" type="text" class="form-control" name="nome" value="{{ $exposicao->nome }}" required autofocus>
 
-                    @if ($errors->has('nome'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('nome') }}</strong>
-                        </span>
-                    @endif
-                </div>
-            </div>
+                            @if ($errors->has('nome'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('nome') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
 
-            <div class="form-group{{ $errors->has('descricao') ? ' has-error' : '' }}">
-                <label for="descricao" class="col-md-4 control-label">Descrição</label>
+                    <div class="form-group{{ $errors->has('descricao') ? ' has-error' : '' }}">
+                        <label for="descricao" class="col-md-4 control-label">Descrição</label>
 
-                <div class="col-md-6">
-                    <input id="descricao" type="text" class="form-control" name="descricao" value="{{ $exposicao->descricao }}" required autofocus>
+                        <div class="col-md-6">
+                            <input id="descricao" type="text" class="form-control" name="descricao" value="{{ $exposicao->descricao }}" required autofocus>
 
-                    @if ($errors->has('descricao'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('descricao') }}</strong>
-                        </span>
-                    @endif
-                </div>
-            </div>
+                            @if ($errors->has('descricao'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('descricao') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
 
-            <div class="form-group{{ $errors->has('imagem') ? ' has-error' : '' }}">
-                <label for="imagem" class="col-md-4 control-label">Imagem</label>
+                    <div class="form-group{{ $errors->has('imagem') ? ' has-error' : '' }}">
+                        <label for="imagem" class="col-md-4 control-label">Imagem</label>
 
-                <div class="col-md-6">
-                    <input id="imagem" type="file" class="form-control" name="imagem" value="{{ $exposicao->imagem }}" required>
+                        <div class="col-md-6">
+                            <input id="imagem" type="file" class="form-control" name="imagem" value="{{ $exposicao->imagem }}" required>
 
-                    @if ($errors->has('imagem'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('imagem') }}</strong>
-                        </span>
-                    @endif
-                </div>
-            </div>
+                            @if ($errors->has('imagem'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('imagem') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
 
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary">
-                        Gravar Alteração
-                    </button>
-                </div>
-            </div>
-        </form>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                Gravar Alteração
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
             <!-- /.panel-body -->
         </div>
