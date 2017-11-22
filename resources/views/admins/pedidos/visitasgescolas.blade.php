42d367d40dc30498543e06dbb778797c7f82806b
Nélio Mavie <neliomelmavie@gmail.com>
2017-11-22 23:17:39 +0200

    Trabalhando o template do admin7

--- resources/views/admins/pedidos/visitasgescolas.blade.php
+++ resources/views/admins/pedidos/visitasgescolas.blade.php
@@ -1,107 +1,107 @@
-@extends('master-admin')
+@extends('layouts.master-admin')
 
 @section('content-admin')
 
-    <div class="row">
-        <div class="col-lg-10">
-            <h1 class="page-header">Visita Guiada Escolar</h1>
-        </div>
-
+<div class="row">
+    <div class="col-lg-10">
+        <h1 class="page-header">Visita Guiada Escolar</h1>
     </div>
 
-    <!-- /.row -->
+</div>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Lista de Eventos
-                </div>
+<!-- /.row -->
 
-                <!-- /.panel-heading -->
-                <div class="panel-body">
-                    <table width="100%" class="table table-striped table-bordered table-hover table-light"
-                           id="dataTables-example">
-                        <thead>
-
-                        <tr>
-                            <th>Nome Da Escola</th>
-                            <th>Email</th>
-                            <th>Responsavel</th>
-                            <th>Data</th>
-                            <th>Hora</th>
-                            <th>Data De Submissão</th>
-                            <th>Descrição</th>
-
-                        </tr>
-
-                        </thead>
-
-                        <tbody>
-
-                        @foreach($pedidos as $p)
-                            <tr class="odd gradeX">
-                                <td>{{$p->nome}}</td>
-                                <td>{{$p->email}}</td>
-                                <td>{{$p->nome_responsavel}}</td>
-                                {{--<td>--}}
-                                    {{--<img style="max-width: 40px;" src="/evento_upload/{{$evento->imagem}}">--}}
-                                {{--</td>--}}
-
-                                <td>{{$p->data}}</td>
-
-                                <td>{{$p->hora}}</td>
-                                <td>{{$p->created_at}}</td>
-                                <td>
-                                    <a href="#">
-                                        <!-- Button trigger modal -->
-                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
-                                          Ver Descrição <i class="fa fa-eye"></i>
-                                        </button>
-
-                                        <!-- Modal -->
-                                        <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                                          <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                              <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLongTitle">Descrição Do Pedido</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                  <span aria-hidden="true">&times;</span>
-                                                </button>
-                                              </div>
-                                              <div class="modal-body">
-                                                    {{$p->descricao}}.. 
-                                              </div>
-                                              <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
-                                              </div>
-                                            </div>
-                                          </div>
-                                        </div>
-                                    </a>
-                                </td>
-
-
-                                {{--<td>--}}
-                                    {{--<a href="/admin/eventos/{{ $evento->id }}/edit">--}}
-                                        {{--<button class="btn btn-success">Editar</button>--}}
-                                    {{--</a>--}}
-                                    {{--<button class="btn btn-danger">Apagar</button>--}}
-                                {{--</td>--}}
-                            </tr>
-                        @endforeach
-
-                        </tbody>
-
-                    </table>
-                    <!-- /.table-responsive -->
-
-                </div>
-                <!-- /.panel-body -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Lista de Eventos
             </div>
-            <!-- /.panel -->
-        </div>
-        <!-- /.col-lg-12 -->
-    </div>
+
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+                <table width="100%" class="table table-striped table-bordered table-hover table-light"
+                id="dataTables-example">
+                <thead>
+
+                    <tr>
+                        <th>Nome Da Escola</th>
+                        <th>Email</th>
+                        <th>Responsavel</th>
+                        <th>Data</th>
+                        <th>Hora</th>
+                        <th>Data De Submissão</th>
+                        <th>Descrição</th>
+
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                    @foreach($pedidos as $p)
+                    <tr class="odd gradeX">
+                        <td>{{$p->nome}}</td>
+                        <td>{{$p->email}}</td>
+                        <td>{{$p->nome_responsavel}}</td>
+                        {{--<td>--}}
+                            {{--<img style="max-width: 40px;" src="/evento_upload/{{$evento->imagem}}">--}}
+                        {{--</td>--}}
+
+                        <td>{{$p->data}}</td>
+
+                        <td>{{$p->hora}}</td>
+                        <td>{{$p->created_at}}</td>
+                        <td>
+                            <a href="#">
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                                  Ver Descrição <i class="fa fa-eye"></i>
+                              </button>
+
+                              <!-- Modal -->
+                              <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                                  <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Descrição Do Pedido</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                          <span aria-hidden="true">&times;</span>
+                                      </button>
+                                  </div>
+                                  <div class="modal-body">
+                                    {{$p->descricao}}.. 
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </td>
+
+
+            {{--<td>--}}
+                {{--<a href="/admin/eventos/{{ $evento->id }}/edit">--}}
+                    {{--<button class="btn btn-success">Editar</button>--}}
+                {{--</a>--}}
+                {{--<button class="btn btn-danger">Apagar</button>--}}
+            {{--</td>--}}
+        </tr>
+        @endforeach
+
+    </tbody>
+
+</table>
+<!-- /.table-responsive -->
+
+</div>
+<!-- /.panel-body -->
+</div>
+<!-- /.panel -->
+</div>
+<!-- /.col-lg-12 -->
+</div>
 @stop
--- resources/views/admins/usuario/perfil.blade.php
+++ resources/views/admins/usuario/perfil.blade.php
@@ -10,99 +10,99 @@
 {{--@endforeach--}}
 
 
-@extends('master-admin')
+@extends('layouts.master-admin')
 
 @section('content-admin')
 
-    <div class="row">
-        <div class="col-lg-10">
-            <h3 class="page-header">Actualizando o Perfil</h3>
-        </div>
-
+<div class="row">
+    <div class="col-lg-10">
+        <h3 class="page-header">Actualizando o Perfil</h3>
     </div>
 
-    <!-- /.row -->
+</div>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Meu Perfil
-                </div>
+<!-- /.row -->
 
-                <!-- /.panel-heading -->
-                <div class="panel-body">
-
-                    <form  method="post"  action="{{ url('usuario', $usuario->id) }}">
-                        {!! csrf_field() !!}
-
-                        <input type="hidden" name="_method" value="PUT">
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="nome">Nome</label>
-                                <input type="text" class="form-control" id="nome" value="{{$usuario->name}}" readonly name="name">
-                            </div>
-
-                            <div class="form-group col-md-6">
-                                <label for="username">Username</label>
-                                <input type="text" class="form-control" id="username" placeholder="" name="username"
-                                       value="{{$usuario->username}}">
-
-                            </div>
-                        </div>
-
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" readonly   value="{{$usuario->email}}">
-
-                            </div>
-
-                            <div class="form-group col-md-4">
-                                <label for="telefone">Nr Celular</label>
-                                <input type="text" name="telefone" id="telefone" class="form-control"   value="{{$usuario->telefone}}">
-
-                            </div>
-
-                            <div class="form-group col-md-2">
-                                <label for="inputid">ID</label>
-                                <input type="text" class="form-control" id="inputid" readonly value="{{$usuario->id}}">
-                            </div>
-                        </div>
-
-
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="nome">Endereço</label>
-                                <input type="text" class="form-control" id="endereco" placeholder="Endereco"
-                                       value="{{$usuario->endereco}}" name="enedereco">
-                            </div>
-
-                            <div class="form-group col-md-4">
-                                <label for="sexo">Sexo</label>
-                                <select class="form-control" id="sexo">
-
-                                    <option value="Masculino">Masculino</option>
-                                    <option value="Feminino">Feminino</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-2">
-                                <label for="nome">Sexo</label>
-                                <input type="text" class="form-control" id="sexo" placeholder="Sexo Actual"  value="{{$usuario->sexo}}" name="" readonly>
-
-                            </div>
-
-                        </div>
-
-
-                        <button type="submit" class="btn btn-primary">Actualizar</button>
-
-                    </form>
-                </div>
-                <!-- /.panel-body -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Meu Perfil
             </div>
-            <!-- /.panel -->
+
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+
+                <form  method="post"  action="{{ url('usuario', $usuario->id) }}">
+                    {!! csrf_field() !!}
+
+                    <input type="hidden" name="_method" value="PUT">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="nome">Nome</label>
+                            <input type="text" class="form-control" id="nome" value="{{$usuario->name}}" readonly name="name">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control" id="username" placeholder="" name="username"
+                            value="{{$usuario->username}}">
+
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" readonly   value="{{$usuario->email}}">
+
+                        </div>
+
+                        <div class="form-group col-md-4">
+                            <label for="telefone">Nr Celular</label>
+                            <input type="text" name="telefone" id="telefone" class="form-control"   value="{{$usuario->telefone}}">
+
+                        </div>
+
+                        <div class="form-group col-md-2">
+                            <label for="inputid">ID</label>
+                            <input type="text" class="form-control" id="inputid" readonly value="{{$usuario->id}}">
+                        </div>
+                    </div>
+
+
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="nome">Endereço</label>
+                            <input type="text" class="form-control" id="endereco" placeholder="Endereco"
+                            value="{{$usuario->endereco}}" name="enedereco">
+                        </div>
+
+                        <div class="form-group col-md-4">
+                            <label for="sexo">Sexo</label>
+                            <select class="form-control" id="sexo">
+
+                                <option value="Masculino">Masculino</option>
+                                <option value="Feminino">Feminino</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-2">
+                            <label for="nome">Sexo</label>
+                            <input type="text" class="form-control" id="sexo" placeholder="Sexo Actual"  value="{{$usuario->sexo}}" name="" readonly>
+
+                        </div>
+
+                    </div>
+
+
+                    <button type="submit" class="btn btn-primary">Actualizar</button>
+
+                </form>
+            </div>
+            <!-- /.panel-body -->
         </div>
-        <!-- /.col-lg-12 -->
+        <!-- /.panel -->
     </div>
+    <!-- /.col-lg-12 -->
+</div>
 @stop
